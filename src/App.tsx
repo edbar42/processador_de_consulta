7cca6a98d6ca856284af7c8818a544ddfa0b0ca6
Eduardo Barroso <edbar42@proton.me>
2026-04-09 21:39:56 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Processador de Consultas</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/helpers/sqlParser.ts
@@ -0,0 +1,3 @@
+export default function sqlParser(query: string) {
+  return query;
+}
